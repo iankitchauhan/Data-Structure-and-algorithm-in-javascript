44ae367b36a0732b7c93672eca2d0ebec73b0460
ankit kumar chauhan <akc66404@gmail.com>
2021-05-16 19:45:07 +0530

    Create double-linkedlist-set-get-method.js

---
+++ double-linkedlist-set-get-method.js
@@ -0,0 +1,135 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoubleLinkedList{
+
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length=0;
+    }
+
+
+    push(val){
+        let a = new Node(val);
+     if(this.length == 0)  {
+         this.head =a;
+         this.tail=a;
+     } else{
+         this.tail.next = a;
+         a.prev = this.tail;
+         this.tail =a;
+     }
+this.length++;
+return this;
+    }
+
+    pop(){
+if(!this.head){
+    return undefined
+}
+let node = this.tail;
+
+if(this.length ===1){
+    this.head = null;
+    this.tail =null;
+} else {
+this.tail = node.prev;
+this.tail.next = null;
+    }
+    node.prev = null;
+this.length--;
+return node;
+    }
+
+    shift(){
+        if(!this.head || this.length == 0){
+            return undefined;
+        }
+                      let node = this.head;
+
+        if(this.length == 1){
+            this.head = null;
+    this.tail =null;
+        } else {
+        this.head = this.head.next;
+        this.head.prev =null;
+        node.next = null;
+        }
+
+        this.length--;
+      
+        return node;
+    }
+
+    unshift(val){
+
+        if(!this.head || this.length == 0){
+            return this.push(val)
+        }
+        let node  = new Node(val);
+       this.head.prev = node;
+       node.next = this.head;
+       this.head = node;
+       this.length++;
+       return this;
+    }
+    get(index){
+        if(index<0 || index >=this.length){
+            return undefined;
+        }
+        if(index < this.length/2) {
+             console.log('from start')
+           var counter =0;
+       var current = this.head;
+        while(counter <index){
+          current =   current.next;
+          counter++;
+        }
+
+      
+        } else{
+            console.log('from end')
+            var counter = this.length-1;
+            current = this.tail;
+            while(counter != index) {
+current = current.prev;
+counter --;
+            }
+
+        }
+         return current;  
+
+    }
+
+
+    set(val,index){
+          if(index<0 || index >=this.length){
+            return undefined;
+        }
+        var node = this.get(index);
+        if(node) {
+              node.val = val;
+            return this;    
+        }
+
+        return false;
+     
+       
+    }
+
+}
+let dll = new DoubleLinkedList();
+dll.push(3);
+dll.push(4);
+dll.push(5);
+dll.push(6);
+dll.push(2);
+dll.get(4);
+dll.set(9,0);
+
