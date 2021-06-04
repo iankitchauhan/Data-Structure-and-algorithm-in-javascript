72095f27b721e5d27f8f996cca711312828f533f
ankit kumar chauhan <akc66404@gmail.com>
2021-06-04 15:06:46 +0530

    Update zy-binary-heap-extract-max-tree.js

--- zy-binary-heap-extract-max-tree.js
+++ zy-binary-heap-extract-max-tree.js
@@ -1,68 +1,70 @@
-class MaxBinaryHeap {
-  constructor() {
-    this.values = [55, 39,41, 18, 27,12, 33]
+class MaxbinaryHeap{
+  constructor(){
+    this.values =[41,39,33,18,27];
   }
 
-  insert(val) {
+
+  insert(val){
     this.values.push(val);
     this.bubbleUp()
   }
 
-  bubbleUp() {
-    let idx = this.values.length - 1;
-    while (true) {
-      let parentIndex = Math.floor((idx - 1) / 2);
-      let parent = this.values[parentIndex];
-      let element = this.values[idx]
-      if (element > parent) {
-        this.values[idx] = parent;
-        this.values[parentIndex] = element;
-        idx = parentIndex;
-      } else {
-        break;
-      }
-    }
-
-  }
-   swap(lr,current){
-     let temp = this.values[current];
-     this.values[current] = this.values[lr];
-     this.values[lr] = temp;
+  bubbleUp(){
+   let idx = this.values.length -1;
+let element = this.values[idx];
+   while(idx>0) {
+     let parentIndex = Math.floor((idx -1)/2);
+     let parent = this.values[parentIndex];
+     if(element <= this.values[parentIndex]) break;
+       this.values[parentIndex] = element;
+       this.values[idx] = parent;
+       idx = parentIndex;
+   
+   }
   }
 
   extractMax(){
-     var removedNode =  this.values.shift();
-      this.values.unshift(this.values[this.values.length-1]);
-      this.values.pop();
-      let idx = 0;
-     let parentValue = this.values[idx];
-      while(true){
-let left = 2*idx+1;
-let right = 2*idx +2;
-if(this.values[left] > this.values[idx] || this.values[right] > this.values[idx]) {
-if(this.values[left] > this.values[right]  || (this.values[left] && !this.values[right])){
-this.swap(left,idx);
-idx = left;
-} else if(this.values[left] < this.values[right]){
-this.swap(right,idx);
-idx = right;
-} else{
-    break;
-}
-} else{
-    break;
-}
-
-      }
-
-
-      return removedNode;
-
+    let max = this.values[0];
+    var oldNode = this.values.pop();
+   
+    if(this.values.length > 0) {
+       this.values[0] =oldNode;
+       console.log(this.values,'values');
+       this.sinkDown()
+    }
+  
+      return max
   }
 
- 
+  sinkDown(){
+    let idx = 0;
+    var element = this.values[idx];
+     while(true){
+        let leftIndex = 2*idx +1;
+        let rightIndex = 2*idx +2;
+        let swap =null;
+        let leftChild = this.values[leftIndex];
+         let rightChild = this.values[rightIndex];
+        if(element < leftChild){
+          swap = leftIndex;
+        }
+         if((swap ==null && element < rightChild) ||
+            (swap !==null && leftChild < rightChild)
+          ){
+          swap = rightIndex;
+        }
+        if(swap === null){
+          break;
+        }
+this.values[idx] = this.values[swap];
+this.values[swap] = element;
+idx=swap;
+
+
+      }
+  }
 }
-
-let heap = new MaxBinaryHeap();
-heap.extractMax()
-
+var heap = new MaxbinaryHeap();
+heap.insert(12);
+heap.insert(55);
+ heap.extractMax();
