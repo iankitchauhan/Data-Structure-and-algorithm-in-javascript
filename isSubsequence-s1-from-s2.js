bac8c17524dbadcf7887ae4958b1792d5e22319a
ankit kumar chauhan <akc66404@gmail.com>
2021-06-07 18:05:35 +0530

    Create isSubsequence-s1-from-s2.js

---
+++ isSubsequence-s1-from-s2.js
@@ -0,0 +1,16 @@
+function isSubsequence(str1, str2) {
+  let i =0;
+    let j =0;
+    while(j<str2.length){
+       if(str2[j] === str1[i]) {
+           i++;
+       }
+       if(i === str1.length) return true;
+       j++;
+    }
+
+    return false;
+
+}
+
+console.log(isSubsequence('sing', 'sting'),'result');
