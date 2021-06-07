39202670901463b38eb7c5d7b14c5f22816c9162
ankit kumar chauhan <akc66404@gmail.com>
2021-06-07 06:44:43 +0530

    Create are-there-duplicates.js

---
+++ are-there-duplicates.js
@@ -0,0 +1,11 @@
+function areThereDuplicates() {
+  let collection = {}
+  for(let val in arguments){
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1;
+  }
+  for(let key in collection){
+    if(collection[key] > 1) return true;
+  }
+  return false;
+}
+areThereDuplicates(1,2,3,1) //return true
